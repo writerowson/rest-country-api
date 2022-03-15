0e82e01b8cebecc0674605a37e57374b7b1da044
coderowson <runurowson@gmail.com>
2022-03-15 20:10:28 +0600

    final

--- country.js
+++ country.js
@@ -11,12 +11,31 @@ const displayCountries = countries => {
     showCountries.innerHTML = allcountriesHTML.join(' ')
 
 }
-const getCountryHTMl = country => {
-    console.log(country.flags.png)
+// const getCountryHTMl = country => {
+//     return `
+//     <div class="country ">
+//     <h2>${country.name.common}</h2>
+//    <img src="${country.flags.png}">
+//     </div>
+//     `
+// }
+// const getCountryHTMl = (country) => {
+//     // ?option o1
+//     const { name, flags } = country
+//     return `
+//     <div class="country ">
+//     <h2>${name.common}</h2>
+//    <img src="${flags.png}">
+//     </div>
+//     `
+// }
+const getCountryHTMl = ({ name, flags, area, capital }) => {
     return `
-    <div class="country container">
-    <h2>${country.name.common}</h2>
-   <img src="${country.flags.png}">
+    <div class="country ">
+    <h2 title="This is a attribute">${name.common}</h2>
+    <h4>Capital : ${capital}</h4>
+    <p>Area : ${area}</p>
+   <img src="${flags.png}">
     </div>
     `
 }
